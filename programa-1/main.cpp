c9de920a33603bd1ed41e67ea906cfc01fae36df
oliversanchez9 <sanchezoliver406@gmail.com>
2025-02-14 08:55:44 -0400

    Add files via upload

---
+++ programa-1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{float PRO;
+printf ("ingrese el promedio del alumno: ");
+scanf ("%f", &PRO);
+if (PRO >= 6)
+    printf("\nAprobado");
+}
